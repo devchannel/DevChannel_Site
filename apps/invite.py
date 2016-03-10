2e6707ff1fc097fb98034082a1781d6c6b144402
g4s-slack <r.mhegyi@gmail.com>
2016-03-10 17:14:49 +0100

    error/notification string linebrakes

--- apps/invite.py
+++ apps/invite.py
@@ -7,7 +7,6 @@ from apps import invite_config
 
 
 def send_invite(email, p_langs):
-    print('ok 1')
     response = requests.post(invite_config.SLACK_INV_URL,
                              params={
                                  'token': invite_config.TOKEN,
@@ -25,18 +24,18 @@ def send_invite(email, p_langs):
             return -1, 'Sorry, invalid email :('
 
         elif err == 'already_in_team':
-            return 1, 'Sorry, it looks like you are already signed up\n' \
+            return 1, 'Sorry, it looks like you are already signed up<br>' \
                       'You will be redirected to devchannel.slack.com'
 
         elif err == 'already_invited':
-            return -1, "It looks like you have already been invited, please look in your mail box (and your spam box too) for an invite.\n" \
-                       "If you can't find it, please contact me on:\n" \
-                       "Reddit: therightman_\n" \
+            return -1, "It looks like you have already been invited, please look in your mail box (and your spam box too) for an invite.<br>" \
+                       "If you can't find it, please contact me on:<br>" \
+                       "Reddit: therightman_<br>" \
                        "Twitter: @therightmandev"
         else:
             return -1, dict_data['error']
 
     elif dict_data['ok']:
         with open("user_info.log", "a") as myfile:
-                myfile.write(email + "-" + p_langs + "\n")
+                myfile.write(email + "-" + p_langs + "<br>")
         return 0, 'Check your mail box :)'
