55c750f43c85a4a3817f41283b54a214fd9a497d
g4s-slack <r.mhegyi@gmail.com>
2016-03-17 22:17:14 +0100

    database decorator added

--- apps/database.py
+++ apps/database.py
@@ -4,52 +4,52 @@ db = TinyDB('database/users.json')
 User = Query()
 
 
-def insert_user(langs='', email='', git='', timezone='', username='', slack_id=''):
-    data = {"email": email, "skills": langs, "github": git, "time": timezone, "name": username, 'slack_id': slack_id}
+def choose_identifier(original):
+    def wrapper(**kwargs):
+        if kwargs.get('slack_id'):
+            kwargs['id_key'] = 'slack_id'
+            kwargs['id_value'] = kwargs.get('slack_id')
+            del kwargs['slack_id']
+        elif kwargs.get('username'):
+            kwargs['id_key'] = 'name'
+            kwargs['id_value'] = kwargs.get('username')
+            del kwargs['username']
+        elif kwargs.get('email'):
+            kwargs['id_key'] = 'email'
+            kwargs['id_value'] = kwargs.get('email')
+            del kwargs['email']
+        else:
+            return 'Please specify email, username or id'
+
+        original(**kwargs)
+    return wrapper
+
+
+def insert_user(langs='', git='', timezone='', email='', username='', slack_id=''):
+    data = {"skills": langs, "github": git, "time": timezone, "email": email, "name": username, 'slack_id': slack_id}
     db.insert(data)
     return 'added: {}'.format(data)
 
 
-def update_user(params, email='', username='', slack_id=''):
-    if email != '':
-        db.update(params, User.email == email)
-        return 'updated {} with: {}'.format(email, params)
-    elif username != '':
-        db.update(params, User.name == name)
-        return 'updated {} with: {}'.format(username, params)
-    elif slack_id != '':
-        db.update(params, User.slack_id == slack_id)
-        return 'updated {} with: {}'.format(slack_id, params)
-    return 'Please specify email, username or id'
+@choose_identifier
+def update_user(params, id_key, id_value):
+    db.update(params, User[id_key] == id_value)
+    return 'updated {} with: {}'.format(id_key, params)
 
 
-def get_user(email='', username='', slack_id=''):
-    if email != '':
-        return str(db.get(User.email == email)) or 'User not found'
-    elif username != '':
-        return str(db.get(User.name == username)) or 'User not found'
-    elif slack_id != '':
-        return str(db.get(User.slack_id == slack_id)) or 'User not found'
-    return 'Please specify email, username or id'
-
+@choose_identifier
+def get_user(id_key, id_value):
+    return str(db.get(User[id_key] == id_value)) or 'User not found'
 
 
 def get_all_users():
     return db.all()
 
 
-def delete_user(email='', username='', slack_id=''):
-    if email != '':
-        db.remove(User.email == email)
-        return 'deleted: {}'.format(email)
-    elif username != '':
-        db.remove(User.name == username)
-        return 'deleted: {}'.format(username)
-    elif slack_id != '':
-        db.remove(User.slack_id == slack_id)
-        return 'deleted: {}'.format(slack_id)
-    return 'Please specify email, username or id'
-
+@choose_identifier
+def delete_user(id_key, id_value):
+    db.remove(User[id_key] == id_value)
+    return 'deleted: {}'.format(id_key)
 
 if __name__ == '__main__':
     pass
