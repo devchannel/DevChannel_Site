23675748f376c735a612326e5686359f43c6fad6
g4s-slack <r.mhegyi@gmail.com>
2016-05-20 16:50:49 +0200

    www non-www routing unified

--- website/__init__.py
+++ website/__init__.py
@@ -1,3 +1,4 @@
+from urllib.parse import urlparse, urlunparse
 import flask
 
 from . import server_config
@@ -6,3 +7,13 @@ app = flask.Flask(__name__)
 app.secret_key = server_config.SERVER_SECRET
 
 from .server import *
+
+
+@app.before_request
+def redir_to_non_www():
+    url = urlparse(flask.request.url)
+    if url.netloc.startswith("www."):
+        url_parts = list(url)
+        url_parts[1] = url_parts[1][4:]
+        k = urlunparse(url_parts)
+        return flask.redirect(k, code=301)
