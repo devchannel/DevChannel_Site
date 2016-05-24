ed042b0c98988373b3fe87bc6196afbb38e1e86b
therightman <therightmandev@gmail.com>
2016-05-24 21:25:36 +0100

    someone help me with /members style, plssss!

--- website/static/js/main.js
+++ website/static/js/main.js
@@ -1,22 +1,35 @@
-var dark_theme = 'css/dark.css';
-var light_theme = 'css/light.css';
+function get_by_order (){
+    url_params = function (){
+        var query = window.location.search.substring(1);
+        var vars = query.split('&');
+        var params = {};
+        for (var i in vars){
+            var pair = vars[i].split('=');
+            params[pair[0]] = decodeURIComponent(pair[1]);
+        }
+    }();
 
-function toggle_theme(){
-    var current_theme = document.getElementById('style').getAttribute('href');
-    if (current_theme==dark_theme){
-        document.getElementById('style').setAttribute('href', light_theme);
-    } else if (current_theme==light_theme){
-        document.getElementById('style').setAttribute('href', dark_theme);
+    document.getElementById("order_dropdown");
+    
+    req_url = 'members?' + 't=' + Math.random()
+
+    var xhttp;
+    if (window.XMLHttpRequest) {
+        xhttp = new XMLHttpRequest();
+    } else {
+        //code for IE 5 and 6
+        xhttp = new ActiveXObject("Microsoft.XMLHTTP");
     }
+
+    xhttp.onreadystatechange = function() {
+        if (xhttp.readyState == 4 && xhttp.status == 200) {
+            console.log(xhttp.responseText.getElementById("order_dropdown"));
+        }
+    }
+
+
+    xhttp.open('GET', req_url, true);
+    xhttp.send();
+
+    return true;
 }
-
-function scroll_check(){
-    var top_scroll = window.pageYOffset;
-
-    /*if (top_scroll > (128)){
-        console.log("maior");
-    }*/
-
-    requestAnimationFrame(scroll_check);
-}
-scroll_check();
