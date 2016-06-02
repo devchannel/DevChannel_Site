a827d832906297b5a1c72be4ac3a067d42794b5b
g4s-slack <r.mhegyi@gmail.com>
2016-06-02 22:19:59 +0200

    index page formatting uses markdown

--- website/utils/article.py
+++ website/utils/article.py
@@ -1,8 +1,5 @@
 import os
 import datetime
-from functools import singledispatch
-from collections import abc
-import html
 
 import tinydb
 
@@ -19,30 +16,6 @@ class Article:
 
     def __repr__(self):
         return str({'author': self.author, 'text': self.text, 'publish_date': self.publish_date})
-
-    def body_to_html(self):
-        html = ''
-        for elem in self.text:
-            html += htmlify(elem) + '\n'
-        return html
-
-
-@singledispatch
-def htmlify(obj):
-    return html.escape(repr(obj))
-
-
-@htmlify.register(str)
-def _(text):
-    content = html.escape(text).replace('\n', '<br><br>\n')
-    return '<p class="announcement">' + content + '</p>'
-
-
-@htmlify.register(tuple)
-@htmlify.register(abc.MutableSequence)
-def _(seq):
-    content = '</li>\n<li>'.join(htmlify(item) for item in seq)
-    return '<ul>\n<li>{}</li>\n</ul>'.format(content)
 
 
 def get_articles():
