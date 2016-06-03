f64ba0dcf42f588563b160ae228379f1c752d293
g4s-slack <r.mhegyi@gmail.com>
2016-06-03 20:42:24 +0200

    added visibility field for posts

--- website/models/posts.py
+++ website/models/posts.py
@@ -9,11 +9,14 @@ def get_curr_date():
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+
+    visible = db.Column(db.Boolean, default=False)
     author = db.Column(db.String(20), default='therightman')
     publish_date = db.Column(db.Date, default=get_curr_date)
     text = db.Column(db.Text)
 
-    def __init__(self, author="", date="", text=""):
+    def __init__(self, visible=None, author="", date="", text=""):
+        self.visible = visible
         self.author = author
         self.publish_date = date
         self.text = text
