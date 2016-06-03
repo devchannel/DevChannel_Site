c5ebeaa8bd653d141979fecf80dd984bab071de8
g4s-slack <r.mhegyi@gmail.com>
2016-06-03 11:54:30 +0200

    admin view css added

--- website/models/admin.py
+++ website/models/admin.py
@@ -4,5 +4,8 @@ from .. import server_config
 
 
 class AdminPost(ModelView):
+    edit_template = 'admin/post.html'
+    create_template = 'admin/post.html'
+
     def is_accessible(self):
         return flask.session.get('username') in server_config.ALLOWED_USERS
--- website/models/posts.py
+++ website/models/posts.py
@@ -3,15 +3,19 @@ import datetime
 from .. import db
 
 
+def get_curr_date():
+    return datetime.datetime.now().date()
+
+
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    author = db.Column(db.String(20))
-    publish_date = db.Column(db.Date)
+    author = db.Column(db.String(20), default='therightman')
+    publish_date = db.Column(db.Date, default=get_curr_date)
     text = db.Column(db.Text)
 
     def __init__(self, author="", date="", text=""):
         self.author = author
-        self.publish_date = date or datetime.datetime.now().date()
+        self.publish_date = date
         self.text = text
 
     def __repr__(self):
